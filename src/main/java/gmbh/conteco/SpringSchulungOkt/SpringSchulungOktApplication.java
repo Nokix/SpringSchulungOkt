ebfb4ce0fdec033781e5033f434419d81ffaa047
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-10-09 10:20:53 +0200

    Initial commit

---
+++ src/main/java/gmbh/conteco/SpringSchulungOkt/SpringSchulungOktApplication.java
@@ -0,0 +1,13 @@
+package gmbh.conteco.SpringSchulungOkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchulungOktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchulungOktApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gmbh/conteco/SpringSchulungOkt/SpringSchulungOktApplicationTests.java
@@ -0,0 +1,13 @@
+package gmbh.conteco.SpringSchulungOkt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchulungOktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
